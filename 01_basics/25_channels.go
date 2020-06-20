774eade56823adf79f6c9e1d5064906609e809e1
Mike <mike.5989@hotmail.com>
2020-06-20 18:09:24 +0200

    channels

---
+++ 01_basics/25_channels.go
@@ -0,0 +1,41 @@
+// https://www.youtube.com/watch?v=S11VFAMEs6E
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func foo(c chan int, someValue int) {
+	defer wg.Done()
+	c <- someValue * 5 //send value to channel
+}
+
+func main() {
+	fooVal := make(chan int, 10) // buffer for 10 items (same as for loop under i < 10)
+
+	// go foo(fooVal, 5)
+	// go foo(fooVal, 3)
+
+	// v1 := <-fooVal
+	// v2 := <-fooVal
+	// fmt.Println(v1)
+	// fmt.Println(v2)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go foo(fooVal, i)
+	}
+
+	wg.Wait()
+	// to prevent deadlock on channel - we need to close channel
+	close(fooVal)
+
+	for item := range fooVal {
+		fmt.Println(item)
+	}
+
+}
