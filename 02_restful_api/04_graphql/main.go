a5a005f77c2fe93c8fdb444e45045e983ba5e341
miguelsilva5989 <miguelsilva5989@gmail.com>
2020-06-23 15:30:59 +0200

    graphql

--- 02_restful_api/04_graphql/main.go
+++ 02_restful_api/04_graphql/main.go
@@ -8,12 +8,114 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+type Tutorial struct {
+	Id       int
+	Title    string
+	Author   Author
+	Comments []Comment
+}
+
+type Author struct {
+	Name      string
+	Tutorials []int
+}
+
+type Comment struct {
+	Body string
+}
+
+func populate() []Tutorial {
+	author := &Author{Name: "Mike", Tutorials: []int{1}}
+	tutorial := Tutorial{
+		Id:     1,
+		Title:  "Go GraphQL Tutorial",
+		Author: *author,
+		Comments: []Comment{
+			Comment{
+				Body: "First comment",
+			},
+		},
+	}
+	var tutorials []Tutorial
+	tutorials = append(tutorials, tutorial)
+
+	return tutorials
+}
+
 func main() {
+	tutorials := populate()
+
+	var commentType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Comment",
+			Fields: graphql.Fields{
+				"body": &graphql.Field{
+					Type: graphql.String,
+				},
+			},
+		},
+	)
+
+	var authorType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Author",
+			Fields: graphql.Fields{
+				"Name": &graphql.Field{
+					Type: graphql.String,
+				},
+				"Tutorials": &graphql.Field{
+					Type: graphql.NewList(graphql.Int),
+				},
+			},
+		},
+	)
+
+	var tutorialType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Tutorial",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.Int,
+				},
+				"title": &graphql.Field{
+					Type: graphql.String,
+				},
+				"author": &graphql.Field{
+					Type: authorType,
+				},
+				"comments": &graphql.Field{
+					Type: graphql.NewList(commentType),
+				},
+			},
+		},
+	)
+
 	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
+		"tutorial": &graphql.Field{
+			Type:        tutorialType,
+			Description: "Get Tutorial by Id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "World", nil
+				id, ok := p.Args["id"].(int)
+				if ok {
+					for _, tutorial := range tutorials {
+						if int(tutorial.Id) == id {
+							return tutorial, nil
+						}
+					}
+				}
+				return nil, nil
+			},
+		},
+		"list": &graphql.Field{
+			Type:        graphql.NewList(tutorialType),
+			Description: "Get full tutorial list",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return tutorials, nil
 			},
 		},
 	}
@@ -28,9 +130,23 @@ func main() {
 		log.Fatalf("Failed to createa new GraphQL Schema, err %v", err)
 	}
 
+	// query := `
+	// 	{
+	// 		list {
+	// 			id
+	// 			title
+	// 		}
+	// 	}
+	// `
 	query := `
 		{
-			hello
+			tutorial(id:1) {
+				title
+				author {
+					Name
+					Tutorials
+				}
+			}
 		}
 	`
 
