f6b0e89dedb61390585eaa5e797abe299df1cb95
miguelsilva5989 <miguelsilva5989@gmail.com>
2020-06-23 18:51:18 +0200

    websocket

---
+++ 05_messaging/02_websocket_and_gorilla/main.go
@@ -0,0 +1,68 @@
+//https://www.youtube.com/watch?v=dniVs0xKYKk
+
+//The real beauty of WebSockets is that they use a grand total of 1 TCP connection and all communication is done over this single long-lived TCP connection.
+//This drastically reduces the amount of network overhead required to build real-time applications using WebSockets as there isn’t a constant polling of HTTP endpoints required.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// allow any origin
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client successfully connected...")
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Go websockets -> http://localhost:8080/")
+	setupRoutes()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
